1c0b797861fe0b95f2545f7e94b894458aa16298
FenixAlive <luisangelmf@gmail.com>
2020-01-28 21:53:24 -0600

    funciones de activación

---
+++ funciones_activacion.py
@@ -0,0 +1,94 @@
+#elaborado por Luis Angel Muñoz Franco
+import numpy as np
+import matplotlib.pyplot as plt
+from math import exp
+
+#declaro funciones
+def escalon(x):
+    res = []
+    for i in x:
+        if i < 0:
+            res.append(0)
+        else:
+            res.append(1)
+    return res
+
+def lineal_tramos(x):
+    res = []
+    for v in x:
+        if v <= 0:
+            res.append(0)
+        elif v >= 1:
+            res.append(1)
+        else:
+            res.append(v)
+    return res
+
+def sigmoidal(x):
+    a = 2
+    res = []
+    for v in x:
+        res.append(1/(1+exp(-a*v)))
+    return res
+
+def signo(x):
+    res = []
+    for v in x:
+        if v < 0:
+            res.append(-1)
+        elif v > 0:
+            res.append(1)
+        else:
+            res.append(0)
+    return res
+
+def lineal(x):
+    A = 1
+    res = []
+    for v in x:
+        res.append(A*v)
+    return res
+
+def gaussiana(x):
+    A = 1
+    B = 3
+    res = []
+    for v in x:
+        res.append(A*exp(-B*v**2))
+    return res
+
+def sinusoidal(x):
+    A = 3
+    B = 2
+    C = 1
+    return A*np.sin(B*x+C)
+
+#declaro espacio de la variable independiente
+x = np.linspace(-3,3,100)
+
+#ploteo
+print("\n\t Funciones de Activación: ")
+print("\n1-Escalón")
+print("2-Lineal a tramos")
+print("3-Sigmoidal")
+print("4-Signo")
+print("5-Lineal")
+print("6-Gaussiana")
+print("7-Sinusoidal")
+opc = input("\nElige una del 1 al 7: ")
+if opc == "1":
+    plt.plot(x, escalon(x))
+elif opc == "2":
+    plt.plot(x, lineal_tramos(x))
+elif opc == "3":
+    plt.plot(x, sigmoidal(x))
+elif opc == "4":
+    plt.plot(x, signo(x))
+elif opc == "5":
+    plt.plot(x, lineal(x))
+elif opc == "6":
+    plt.plot(x, gaussiana(x))
+elif opc == "7":
+    plt.plot(x, sinusoidal(x))
+
+plt.show()
